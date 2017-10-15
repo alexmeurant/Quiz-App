17d7e9e64c8e50f12524dd252ffcc096e3bb109a
MEURANT <alexandremeurant@hotmail.fr>
2017-10-15 17:41:50 +0200

    Declare member variables

--- QuizApp/app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Declare member variables and connect to xml views:
+        TextView mWelcomeTextView = (TextView) findViewById(R.id.WelcomeTextView);
+        EditText mNameInput = (EditText) findViewById(R.id.NameInput);
+        Button mPlayButton = (Button) findViewById(R.id.PlayButton);
     }
 }
