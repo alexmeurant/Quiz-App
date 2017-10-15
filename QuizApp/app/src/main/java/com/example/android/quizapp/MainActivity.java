a4428d24f97854c242eb95c708cbf8c5c984ddf9
MEURANT <alexandremeurant@hotmail.fr>
2017-10-15 18:07:21 +0200

    Enbled button only when player has written his name

--- QuizApp/app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.android.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import static android.R.attr.editable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +20,27 @@ public class MainActivity extends AppCompatActivity {
 
         // Declare member variables and connect to xml views:
         TextView mWelcomeTextView = (TextView) findViewById(R.id.WelcomeTextView);
-        EditText mNameInput = (EditText) findViewById(R.id.NameInput);
-        Button mPlayButton = (Button) findViewById(R.id.PlayButton);
+        final EditText mNameInput = (EditText) findViewById(R.id.NameInput);
+        final Button mPlayButton = (Button) findViewById(R.id.PlayButton);
+
+        // Play button is not enabled till player does not write his name:
+        mPlayButton.setEnabled(false);
+
+        mNameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                mPlayButton.setEnabled(charSequence.toString().length() != 0);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
