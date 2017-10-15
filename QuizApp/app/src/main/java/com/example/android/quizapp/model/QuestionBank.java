48cb754d972a3506805d7363e91d046395bb2d1e
MEURANT <alexandremeurant@hotmail.fr>
2017-10-15 20:43:30 +0200

    Added variables, constructor and getQuestion method

--- QuizApp/app/src/main/java/com/example/android/quizapp/model/QuestionBank.java
+++ QuizApp/app/src/main/java/com/example/android/quizapp/model/QuestionBank.java
@@ -1,5 +1,32 @@
 package com.example.android.quizapp.model;
 
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class QuestionBank {
+
+    private List<Question> mQuestionList;
+    private int mNextQuestionIndex;
+
+
+    public QuestionBank(List<Question> questionList) {
+        mQuestionList = questionList;
+
+        // shuffle the question list:
+        Collections.shuffle(mQuestionList);
+
+        mNextQuestionIndex = 0;
+    }
+
+    public Question getQuestion(){
+        // Ensure we loop over the questions:
+        if (mNextQuestionIndex == mQuestionList.size()) {
+            mNextQuestionIndex = 0;
+        }
+
+        // Please note the post-incrementation:
+        return mQuestionList.get(mNextQuestionIndex++);
+    }
 }
